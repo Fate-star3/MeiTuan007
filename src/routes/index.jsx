bcc09e597d8ff1a04504dbc207173f326b1c519b
Fate-star3 <1647749125@qq.com>
2022-07-31 21:53:43 +0800

    over

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -20,7 +20,7 @@ const RoutesConfig = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<Home /> }></Route>
             <Route path="/home" element={<Home />}></Route>
             {/* <Route path="/member" element={<Member />}></Route> */}
             <Route path="/order" element={<Order />}></Route>
